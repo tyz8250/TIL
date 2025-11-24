5f8f07a57480195a678bcdfd82c2c37e1a532e5b
tyz8250 <tyz0308@gmail.com>
2025-11-25 07:38:38 +0900

    add

--- Go/CARDS/deck.go
+++ Go/CARDS/deck.go
@@ -41,3 +41,16 @@ func (d deck) toString() string{
 func (d deck) saveToFile(filename string) error{
 	return os.WriteFile(filename, []byte(d.toString()), 0666)
 }
+
+func newDeckFromFile(filename string) deck{
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		// log the error and return a call to newDeck()
+		// log the error and entirely quit the program
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s:= strings.Split(string(bs), ",")
+	return  deck(s)
+}
