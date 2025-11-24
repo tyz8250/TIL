6ba38ff9258c37d3411705e07490356d7a6bd221
tyz8250 <tyz0308@gmail.com>
2025-11-24 17:11:18 +0900

    add

--- Go/CARDS/main.go
+++ Go/CARDS/main.go
@@ -1,16 +1,12 @@
 package main
 
-import "fmt"
-
 func main () {
-	cards := [] string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	for i ,card := range cards{
-		fmt.Println(i,card)
-	}
+	cards.print()
 }
 
 func newCard () string {
-	return "Ace of Spade"
+	return "Five of Diamonds"
 }
