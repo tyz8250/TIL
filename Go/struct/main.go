0e59a167731fe589f64b8c61ecc9d73603da8bbf
tyz8250 <tyz0308@gmail.com>
2025-11-30 15:07:40 +0900

    add

--- Go/struct/main.go
+++ Go/struct/main.go
@@ -1,10 +1,15 @@
 package main
 
+import "fmt"
+
 type person struct{
 	firstName string
 	lastName string
 }
 
 func main(){
+	// structに型を入れている。
+	alex := person{firstName:"Alex", lastName: "Anderson"}
+	fmt.Println(alex)
 
 }
