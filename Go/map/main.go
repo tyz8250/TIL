211a85257fe821982cf69542fab25b74f95e1a19
tyz8250 <tyz0308@gmail.com>
2025-12-01 20:56:38 +0900

    add

--- Go/map/main.go
+++ Go/map/main.go
@@ -12,7 +12,7 @@ func main() {
 	printMap(colors)
 }
 
-func printMap(c map[string]string){
+func printMap(c map[string]string) {
 	for color, hex := range c {
 		fmt.Println("Hex code for", color, "is", hex)
 	}
