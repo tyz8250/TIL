58df37a3f010f422456bb17850cbe28cb4d1fcd5
tyz8250 <tyz0308@gmail.com>
2025-12-07 20:54:10 +0900

    add

---
+++ Go/CHANNELS/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	link := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"https://golang.org",
+		"http://amazon.com"
+	}
+	for _, link := range links{
+	} 
+}
