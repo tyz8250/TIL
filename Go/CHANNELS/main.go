3cf1d5a39e193db2cbdf7f816f46efc6561695de
tyz8250 <tyz0308@gmail.com>
2025-12-08 20:59:38 +0900

    add

--- Go/CHANNELS/main.go
+++ Go/CHANNELS/main.go
@@ -1,13 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+// 1つずつ順番にリンクの状態をチェックする。並行処理ではない。
 func main() {
-	link := []string{
+	links := []string{
 		"http://google.com",
 		"http://facebook.com",
 		"http://stackoverflow.com",
 		"https://golang.org",
 		"http://amazon.com",
 	}
-	for _, link := range link{
+	for _, link := range links {
+		checkLink(link)
 	} 
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	}
+	fmt.Println(link, "is up!")
 }
